75a66de9d8673b5cb741318be1a837b08722d556
Hongjun Choi <hong_jun_choi@brown.edu>
2016-04-15 06:12:43 -0400

    sync

--- config/env/development.js
+++ config/env/development.js
@@ -5,6 +5,7 @@ module.exports = {
    // grunt db-reset:development
    // That will create the local nodegoat data-store, or restore it to a clean state if it already exists.
 
-   //db: "mongodb://localhost:27017/nodegoat",
+   db: "mongodb://hc121:scottchoi92@ds023530.mlab.com:23530/heroku_xvhbfhzz",
+
 
 };
